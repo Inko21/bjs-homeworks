3275ad8452f8b56de21df5c9f2098d71f1ea525d
Pavel Novikov <pchela57@inbox.ru>
2019-11-05 14:47:30 +0400

    homework done init

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -1,4 +1,4 @@
-
+'use strict';
 
 function calculateQuadraticEquation(){
     let a = +window.a.value;
@@ -11,9 +11,24 @@ function calculateQuadraticEquation(){
 }
 
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    //return x;
-}
+// Задание 1
+  let x = [];
+  let dis = (b ** 2) - 4 * a * c;
+  if (dis < 0) {
+    return x;
+  };
+  if (dis === 0) {
+    let y = -b / (2 * a);
+    x.push(y);
+    return x;
+  };
+  if (dis > 0) {
+    let y1 = (-b + Math.sqrt(dis)) / (2 * a);
+    let y2 = (-b - Math.sqrt(dis)) / (2 * a);
+    x.push(y1, y2);
+    return x;
+  };
+};
 
 function calculateAverageRating(){
     let marks = window.marks.value.split("").map(Number).filter((n)=> !isNaN(n) && n > 0);
@@ -22,8 +37,18 @@ function calculateAverageRating(){
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    //return averageMark;
+  // код для задачи №2 писать здесь
+  let averageMark = 0;
+  let counter = 0;
+  if (marks.length > 5) {
+    console.log('Количество оценок более 5');
+    marks = marks.splice(5);
+  };
+  for (let i = 0; i < marks.length; i++) {
+    counter += marks[i]; 
+  };
+  console.log(`Средняя отметка: ${averageMark}`)
+  return averageMark = Math.round(counter / marks.length);
 }
 
 function calculateDrinkTask(){
@@ -35,6 +60,16 @@ function calculateDrinkTask(){
 
 function askDrink(name,dateOfBirthday){
     // код для задачи №3 писать здесь
-    //console.log(result)
-    //return result;
+  let date = dateOfBirthday.getFullYear();
+  let currentDay = new Date();
+  let currentYear = currentDay.getFullYear();
+  let age = currentYear - date;
+  let result = '';
+  if (age >= 18) {
+    result =`Не желаете ли олд-фэшн, ${name} ?`;
+  } else {
+      result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    }
+  console.log(result);
+  return result;
 }
