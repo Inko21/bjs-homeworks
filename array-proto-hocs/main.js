7e336af3c444ea2786dc2a51292fae88af871add
Pavel Novikov <pchela57@inbox.ru>
2019-11-20 13:12:27 +0400

    second task

--- array-proto-hocs/main.js
+++ array-proto-hocs/main.js
@@ -1,5 +1,5 @@
 'use strict'
-
+// задание 1
 function compareArrays(arr1, arr2){
   if (arr1.length !== arr2.length){
     return false;
@@ -16,3 +16,30 @@ function compareArrays(arr1, arr2){
 // Альтернативный вариант решения
 
 const compareArray = (arr1, arr2) => arr1.length !== arr2.length ? false : arr1.every((num, i) => num === arr2[i]);
+
+const mas1 = [1, 2, 3];
+const mas2 = [1, 2, 3];
+compareArray(mas1, mas2);
+
+// задание 2
+
+function memoize(fn, limit) {
+  const memory = [];
+  return function () {
+    let result = 0;
+    let argumentsFunction = Array.from(arguments);
+    let elementCheck = memory.find(function (element, i){
+      return element(argumentsFunction, memory[i].argumentsFunction);
+    });
+    if (elementCheck === undefined) {
+      let result = fn(...argumentsFunction);
+      memory.push({argumentsFunction, result});
+    } else {
+      result = elementCheck.result;
+    }
+    if (memory.length > (limit)) {
+      memory.shift();
+    }
+      return result;
+  }
+};
